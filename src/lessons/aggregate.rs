3b8c4e172638f50873f0e7815035c2e26b1bbe6a
Sean Smith <sean.smith@tenx.tech>
2019-02-17 01:03:38 +0800

    Return all lesson content in GET endpoint

--- src/lessons/aggregate.rs
+++ src/lessons/aggregate.rs
@@ -1,9 +1,30 @@
+use super::types::Lesson;
 use super::lesson_01;
 use super::lesson_02;
-use super::types::Lesson;
+use super::lesson_03;
+use super::lesson_04;
+use super::lesson_05;
+use super::lesson_06;
+use super::lesson_07;
+use super::lesson_08;
+use super::lesson_09;
+use super::lesson_10;
+use super::lesson_11;
 
 pub fn get_content() -> Vec<Lesson> {
-    vec![lesson_01::get_content(), lesson_02::get_content()]
+    vec![
+        lesson_01::get_content(),
+        lesson_02::get_content(),
+        lesson_03::get_content(),
+        lesson_04::get_content(),
+        lesson_05::get_content(),
+        lesson_06::get_content(),
+        lesson_07::get_content(),
+        lesson_08::get_content(),
+        lesson_09::get_content(),
+        lesson_10::get_content(),
+        lesson_11::get_content(),
+    ]
 }
 
 #[cfg(test)]
--- src/lessons/mod.rs
+++ src/lessons/mod.rs
@@ -1,5 +1,14 @@
 pub mod lesson_01;
 pub mod lesson_02;
+pub mod lesson_03;
+pub mod lesson_04;
+pub mod lesson_05;
+pub mod lesson_06;
+pub mod lesson_07;
+pub mod lesson_08;
+pub mod lesson_09;
+pub mod lesson_10;
+pub mod lesson_11;
 
 pub mod aggregate;
 pub mod types;
