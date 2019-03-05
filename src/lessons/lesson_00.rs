e4f412e70609995c6a51d1e516639ef93f567575
Sean Smith <sean.smith@tenx.tech>
2019-03-05 10:28:52 +0800

    Update

--- src/lessons/lesson_00.rs
+++ src/lessons/lesson_00.rs
@@ -5,127 +5,127 @@ pub fn get_content() -> Lesson {
         Item {
             characters: "What is idemopotency?",
             english: "An idempotent operation can be repeated over and over without changing the result. There are no side effects.",
-            phonetic: "placeholder",
+            phonetic: "",
         },
         Item {
             characters: "What is atomicity?",
             english: "Atomicity refers to a series of operations (or one) such that all occur or none. This is important for database transactions.",
-            phonetic: "placeholder",
-        },
-        Item {
-            characters: "",
-            english: "",
             phonetic: "",
         },
         Item {
-            characters: "",
-            english: "",
+            characters: "What is reliability?",
+            english: "Reliability is if a system can remain operational if services/components fail.",
             phonetic: "",
         },
         Item {
-            characters: "",
-            english: "",
+            characters: "What is the difference between availability and reliability?",
+            english: "An highly available system is not necessarily reliable. Reliability, however, implies availability.",
             phonetic: "",
         },
-        Item {
-            characters: "",
-            english: "",
-            phonetic: "",
-        },
-        Item {
-            characters: "",
-            english: "",
-            phonetic: "",
-        },
-        Item {
-            characters: "",
-            english: "",
-            phonetic: "",
-        },
-        Item {
-            characters: "",
-            english: "",
-            phonetic: "",
-        },
-        Item {
-            characters: "",
-            english: "",
-            phonetic: "",
-        },
-        Item {
-            characters: "",
-            english: "",
-            phonetic: "",
-        },
-        Item {
-            characters: "",
-            english: "",
-            phonetic: "",
-        },
-        Item {
-            characters: "",
-            english: "",
-            phonetic: "",
-        },
-        Item {
-            characters: "",
-            english: "",
-            phonetic: "",
-        },
-        Item {
-            characters: "",
-            english: "",
-            phonetic: "",
-        },
-        Item {
-            characters: "",
-            english: "",
-            phonetic: "",
-        },
-        Item {
-            characters: "",
-            english: "",
-            phonetic: "",
-        },
-        Item {
-            characters: "",
-            english: "",
-            phonetic: "",
-        },
-        Item {
-            characters: "",
-            english: "",
-            phonetic: "",
-        },
-        Item {
-            characters: "",
-            english: "",
-            phonetic: "",
-        },
-        Item {
-            characters: "",
-            english: "",
-            phonetic: "",
-        },
-        Item {
-            characters: "",
-            english: "",
-            phonetic: "",
-        },
-        Item {
-            characters: "",
-            english: "",
-            phonetic: "",
-        },
-        Item {
-            characters: "",
-            english: "",
-            phonetic: "",
-        },
-        Item {
-            characters: "",
-            english: "",
-            phonetic: "",
-        },
+//        Item {
+//            characters: "",
+//            english: "",
+//            phonetic: "",
+//        },
+//        Item {
+//            characters: "",
+//            english: "",
+//            phonetic: "",
+//        },
+//        Item {
+//            characters: "",
+//            english: "",
+//            phonetic: "",
+//        },
+//        Item {
+//            characters: "",
+//            english: "",
+//            phonetic: "",
+//        },
+//        Item {
+//            characters: "",
+//            english: "",
+//            phonetic: "",
+//        },
+//        Item {
+//            characters: "",
+//            english: "",
+//            phonetic: "",
+//        },
+//        Item {
+//            characters: "",
+//            english: "",
+//            phonetic: "",
+//        },
+//        Item {
+//            characters: "",
+//            english: "",
+//            phonetic: "",
+//        },
+//        Item {
+//            characters: "",
+//            english: "",
+//            phonetic: "",
+//        },
+//        Item {
+//            characters: "",
+//            english: "",
+//            phonetic: "",
+//        },
+//        Item {
+//            characters: "",
+//            english: "",
+//            phonetic: "",
+//        },
+//        Item {
+//            characters: "",
+//            english: "",
+//            phonetic: "",
+//        },
+//        Item {
+//            characters: "",
+//            english: "",
+//            phonetic: "",
+//        },
+//        Item {
+//            characters: "",
+//            english: "",
+//            phonetic: "",
+//        },
+//        Item {
+//            characters: "",
+//            english: "",
+//            phonetic: "",
+//        },
+//        Item {
+//            characters: "",
+//            english: "",
+//            phonetic: "",
+//        },
+//        Item {
+//            characters: "",
+//            english: "",
+//            phonetic: "",
+//        },
+//        Item {
+//            characters: "",
+//            english: "",
+//            phonetic: "",
+//        },
+//        Item {
+//            characters: "",
+//            english: "",
+//            phonetic: "",
+//        },
+//        Item {
+//            characters: "",
+//            english: "",
+//            phonetic: "",
+//        },
+//        Item {
+//            characters: "",
+//            english: "",
+//            phonetic: "",
+//        },
     ]
 }
