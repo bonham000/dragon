6816fc51740e7c5058c666f06bdfc472bd3262a1
Sean Smith <sean.smith@tenx.tech>
2019-03-05 11:27:26 +0800

    Add 00 content

--- src/lessons/lesson_00.rs
+++ src/lessons/lesson_00.rs
@@ -22,91 +22,91 @@ pub fn get_content() -> Lesson {
             english: "An highly available system is not necessarily reliable. Reliability, however, implies availability.",
             phonetic: "",
         },
-//        Item {
-//            characters: "",
-//            english: "",
-//            phonetic: "",
-//        },
-//        Item {
-//            characters: "",
-//            english: "",
-//            phonetic: "",
-//        },
-//        Item {
-//            characters: "",
-//            english: "",
-//            phonetic: "",
-//        },
-//        Item {
-//            characters: "",
-//            english: "",
-//            phonetic: "",
-//        },
-//        Item {
-//            characters: "",
-//            english: "",
-//            phonetic: "",
-//        },
-//        Item {
-//            characters: "",
-//            english: "",
-//            phonetic: "",
-//        },
-//        Item {
-//            characters: "",
-//            english: "",
-//            phonetic: "",
-//        },
-//        Item {
-//            characters: "",
-//            english: "",
-//            phonetic: "",
-//        },
-//        Item {
-//            characters: "",
-//            english: "",
-//            phonetic: "",
-//        },
-//        Item {
-//            characters: "",
-//            english: "",
-//            phonetic: "",
-//        },
-//        Item {
-//            characters: "",
-//            english: "",
-//            phonetic: "",
-//        },
-//        Item {
-//            characters: "",
-//            english: "",
-//            phonetic: "",
-//        },
-//        Item {
-//            characters: "",
-//            english: "",
-//            phonetic: "",
-//        },
-//        Item {
-//            characters: "",
-//            english: "",
-//            phonetic: "",
-//        },
-//        Item {
-//            characters: "",
-//            english: "",
-//            phonetic: "",
-//        },
-//        Item {
-//            characters: "",
-//            english: "",
-//            phonetic: "",
-//        },
-//        Item {
-//            characters: "",
-//            english: "",
-//            phonetic: "",
-//        },
+        Item {
+            characters: "What is serviceability/manageability for a system?",
+            english: "How easily system problems can be diagnosed/repaired when they arise/how easily services can be rolled back if necessary.",
+            phonetic: "",
+        },
+        Item {
+            characters: "What is load balancing?",
+            english: "A load balancer distributes request traffic evenly to a series of services to spread the load and allow the system to provide high availability at a large request volume.",
+            phonetic: "",
+        },
+        Item {
+            characters: "Between which system layers can a load balancer be added?",
+            english: "Between clients and web servers, between web servers and caches or application servers, between application servers and databases",
+            phonetic: "",
+        },
+        Item {
+            characters: "What are some load balancing algorithms to distribute requests?",
+            english: "Least connection method (fewest active connections), least response time method, round robin, and so on. All utilize health checks and eliminate failed servers.",
+            phonetic: "",
+        },
+        Item {
+            characters: "What if a load balancer fails?",
+            english: "A load balancer is a single point of failure. Typically two can operate in a cluster and monitor each others health. If the active balancer fails, the passive balancer can take over and the failed server can be replaced.",
+            phonetic: "",
+        },
+        Item {
+            characters: "What are some approaches for cache invalidation for a distributed system?",
+            english: "Write-through cache (data is written to the cache and storage), write-around cache (data is written to storage only - a new request will miss the cache), write-back cache (data is written to the cache only, and later persisted to storage).",
+            phonetic: "",
+        },
+        Item {
+            characters: "What are some cache eviction strategies?",
+            english: "Least frequently used, least recently used, last in first out, first in first out, etc.",
+            phonetic: "",
+        },
+        Item {
+            characters: "What is data sharding/partitioning?",
+            english: "Data sharding is scaling a database layer of an application by distributing a single database across multiple partitions or shards. It's essentially scaling a database horizontally, which is usually required at some data scale.",
+            phonetic: "",
+        },
+        Item {
+            characters: "What is range based sharding, and one is one shortcoming of this approach?",
+            english: "Distributing data based on where some part of it's input falls in a range, e.g. zip codes within a numeric range or user email based on letter range. A shortcoming is this can lead to unbalanced load across the database partitions.",
+            phonetic: "",
+        },
+        Item {
+            characters: "What is key/hash based partitioning?",
+            english: "This approaches takes a key of some input value (e.g. user id), e.g. by simply modding it by the number of partitions, to determine the partition that data will belong to.",
+            phonetic: "",
+        },
+        Item {
+            characters: "What is one problem with hash/key based data partitioning?",
+            english: "It's fixed to the number of database services at one point in time, and cannot easily accommodate additional partitions in the future.",
+            phonetic: "",
+        },
+        Item {
+            characters: "What is one problem with database sharding in terms of querying data?",
+            english: "Joins between multiple data sources or tables can become very expensive and no longer possible. One solution to this is data denormalization, which allows queries which would have required joins to run on a single table. Materialized views is a similar approach, where specific query tables of data are maintained. However, these approaches increase the number of writes required and introduce problems of data consistency.",
+            phonetic: "",
+        },
+        Item {
+            characters: "What is a data integrity issue encountered with database sharding?",
+            english: "You typically cannot support foreign key constraints across different database shards. This means referential integrity must be maintained in application code, or in routine SQL jobs which clean up dangling references.",
+            phonetic: "",
+        },
+        Item {
+            characters: "What is one problem with database sharding?",
+            english: "Shard rebalancing may be required, if data is distributed unevenly or if load on a particular shard is too high. This means adding additional shards and changing the partitioning scheme, or rebalancing existing data, or both - all of which is very hard to do without downtime.",
+            phonetic: "",
+        },
+        Item {
+            characters: "What is database indexing? What are the tradeoffs?",
+            english: "Database indexing means certain keys for input data are indexed so the same data can be immediately looked up by the key. This is why we indexed IOC by type|value at TruSTAR, for query performance. Indexing increase storage space and write time, however, so it's important not to add indexes unnecessarily. In addition, it's important to choose indexes carefully based on how the data will be queried.",
+            phonetic: "",
+        },
+        Item {
+            characters: "What is a proxy server?",
+            english: "An intermediary that stands between clients and backing services. It can be used to filter requests, add logging, add headers, cache responses, handle API rate limiting, etc.",
+            phonetic: "",
+        },
+        Item {
+            characters: "What is a reverse proxy?",
+            english: "A proxy server which stands between a client and returns data on the behalf of one or more servers. To the client, the data appears to come from the proxy server.",
+            phonetic: "",
+        },
 //        Item {
 //            characters: "",
 //            english: "",
