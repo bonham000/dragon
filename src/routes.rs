69bc5c595727716df7c2d563544da6e4b01bd22b
Sean Smith <sean.smith@tenx.tech>
2019-05-19 01:14:39 +0800

    Run cargo fmt

--- src/routes.rs
+++ src/routes.rs
@@ -1,19 +1,22 @@
 use rocket;
-use rocket_contrib::serve::{StaticFiles};
+use rocket_contrib::serve::StaticFiles;
 
 use super::db;
 use super::service;
 
 pub fn build() -> rocket::Rocket {
-    rocket::ignite().manage(db::init_pool()).mount(
-        "/",
-        routes![
-            service::index,
-            service::lessons,
-            service::find_or_create_user,
-            service::set_scores,
-            service::set_experience_points,
-            service::remove_user,
-        ],
-    ).mount("/static", StaticFiles::from("static"))
+    rocket::ignite()
+        .manage(db::init_pool())
+        .mount(
+            "/",
+            routes![
+                service::index,
+                service::lessons,
+                service::find_or_create_user,
+                service::set_scores,
+                service::set_experience_points,
+                service::remove_user,
+            ],
+        )
+        .mount("/static", StaticFiles::from("static"))
 }
