1e38d41130f32781747beaeb5c43e10864e59291
Sean Smith <sean.smith@tenx.tech>
2019-05-19 03:06:59 +0800

    Create lesson_03 content - WIP

--- src/lessons/aggregate.rs
+++ src/lessons/aggregate.rs
@@ -1,9 +1,14 @@
 use super::lesson_01;
 use super::lesson_02;
+use super::lesson_03;
 use super::types::Lesson;
 
 pub fn get_content() -> Vec<Lesson> {
-    vec![lesson_01::get_content(), lesson_02::get_content()]
+    vec![
+        lesson_01::get_content(),
+        lesson_02::get_content(),
+        lesson_03::get_content(),
+    ]
 }
 
 #[cfg(test)]
--- src/lessons/mod.rs
+++ src/lessons/mod.rs
@@ -1,5 +1,6 @@
 pub mod lesson_01;
 pub mod lesson_02;
+pub mod lesson_03;
 
 pub mod aggregate;
 pub mod types;
